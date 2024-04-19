7fad925136eb803e1377ca3775078712281ca6ab
GabrielVDantas <gvdantas2028@gmail.com>
2024-04-19 14:49:05 -0300

    base do projeto

---
+++ src/test/java/br/com/aluraAPI/aluraAPI/AluraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aluraAPI.aluraAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
