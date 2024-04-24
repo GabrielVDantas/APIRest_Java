870f9143803d9a80294e1d0d140a225e09cae8a0
GabrielVDantas <gvdantas2028@gmail.com>
2024-04-24 15:55:22 -0300

    criando entidade e tabela user

---
+++ src/main/java/br/com/aluraAPI/aluraAPI/domain/User/User.java
@@ -0,0 +1,22 @@
+package br.com.aluraAPI.aluraAPI.domain.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "User")
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+}
